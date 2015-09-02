fa39d715005cb3fe138a94c9abf27b092206e9fb
Erik Torsner <erik@torgesta.com>
2015-09-03 01:08:37 +0200

    adding

---
+++ Gruntfile_step3.js
@@ -0,0 +1,96 @@
+module.exports = function(grunt) {
+	var shell = require('shelljs');
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		phpunit: {
+			default: {
+			},
+			options: {
+				logJson: 'build/phpunit.json'
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-phpunit');
+	grunt.loadNpmTasks('grunt-version');
+	
+	grunt.registerTask('default', 'Log some stuff.', function() {
+		ls = getLocalsettings();
+		grunt.log.write('Hear hear, we are up...').ok();
+	});
+
+
+
+	/*
+	* Installing WP
+	*/
+	grunt.registerTask('wp-install', '', function() {
+		ls = getLocalsettings();
+		wpcmd = 'wp --path=' + ls.wppath + ' --allow-root ';
+
+		shell.mkdir('-p', ls.wppath);
+
+		if(!shell.test('-e', ls.wppath + '/wp-config.php')) {
+			shell.exec(wpcmd + 'core download --force');
+			shell.exec(wpcmd + 'core config --dbname=' + ls.dbname + ' --dbuser=' + ls.dbuser + ' --dbpass=' + ls.dbpass + ' --quiet');
+			shell.exec(wpcmd + 'core install --url=' + ls.url + ' --title="Tracker.social" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password"');
+		} else {
+			grunt.log.write('Wordpress is already installed').ok();
+		}
+	});
+
+	/*
+	* Setting up WP, initiating it with content
+	* 
+	*/
+	grunt.registerTask('wp-setup', '', function() {
+		ls = getLocalsettings();
+		wpcmd = 'wp --path=' + ls.wppath + ' --allow-root ';
+		
+		pwd = shell.pwd();
+
+		// some standard plugins
+		shell.exec(wpcmd + 'plugin install --activate if-menu');
+		shell.exec(wpcmd + 'plugin install --activate baw-login-logout-menu');
+		shell.exec(wpcmd + 'plugin install --activate wp-cfm');
+
+		// our own theme(s)
+		themes = ['wordpressapp'];
+		for(i=0;i<themes.length;i++) {
+			name = themes[i];
+			shell.exec('rm -f ' + ls.wppath + '/wp-content/themes/' + name);
+			shell.exec('ln -sf ' + pwd + '/wp-content/themes/' + name + ' ' + ls.wppath + '/wp-content/themes/' + name);
+		}
+		shell.exec(wpcmd + 'theme activate wordpressapp');//
+
+
+		// our own, site-specific plugins
+		plugins = ['wordpressapp'];
+		for(i=0;i<plugins.length;i++) {
+			name = plugins[i];
+			shell.exec('rm -f ' + ls.wppath + '/wp-content/plugins/' + name);
+			shell.exec('ln -sf ' + pwd + '/wp-content/plugins/' + name + ' ' + ls.wppath + '/wp-content/plugins/' +name);
+			shell.exec(wpcmd + 'plugin activate ' + name);
+		}
+
+	});
+
+	function getLocalsettings(test) {
+		var testMode = grunt.option('test');
+		if(test == true) {
+			testMode = true;
+		}
+		ls = grunt.file.readJSON('localsettings.json');
+		if(ls.wppath === undefined) ls.wppath = shell.pwd() + '/www/wordpress-default';
+		if(testMode == true) {
+			ls.environment = 'test';
+			ls.wppath = ls.wppath_test;
+			ls.dbname = ls.dbname_test;
+			ls.url = ls.url_test;
+		}
+		return ls;
+	}
+
+
+};
---
+++ Gruntfile_step4.js
@@ -0,0 +1,121 @@
+module.exports = function(grunt) {
+	var shell = require('shelljs');
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		phpunit: {
+			default: {
+			},
+			options: {
+				logJson: 'build/phpunit.json'
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-phpunit');
+	grunt.loadNpmTasks('grunt-version');
+	
+	grunt.registerTask('default', 'Log some stuff.', function() {
+		ls = getLocalsettings();
+		grunt.log.write('Hear hear, we are up...').ok();
+	});
+
+
+
+	/*
+	* Installing WP
+	*/
+	grunt.registerTask('wp-install', '', function() {
+		ls = getLocalsettings();
+		wpcmd = 'wp --path=' + ls.wppath + ' --allow-root ';
+
+		shell.mkdir('-p', ls.wppath);
+
+		if(!shell.test('-e', ls.wppath + '/wp-config.php')) {
+			shell.exec(wpcmd + 'core download --force');
+			shell.exec(wpcmd + 'core config --dbname=' + ls.dbname + ' --dbuser=' + ls.dbuser + ' --dbpass=' + ls.dbpass + ' --quiet');
+			shell.exec(wpcmd + 'core install --url=' + ls.url + ' --title="WordpressApp" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password"');
+		} else {
+			grunt.log.write('Wordpress is already installed').ok();
+		}
+	});
+
+	/*
+	* Setting up WP, initiating it with content
+	* 
+	*/
+	grunt.registerTask('wp-setup', '', function() {
+		ls = getLocalsettings();
+		wpcmd = 'wp --path=' + ls.wppath + ' --allow-root ';
+		
+		pwd = shell.pwd();
+
+		// some standard plugins
+		shell.exec(wpcmd + 'plugin install --activate if-menu');
+		shell.exec(wpcmd + 'plugin install --activate baw-login-logout-menu');
+		shell.exec(wpcmd + 'plugin install --activate wp-cfm');
+
+		// our own theme(s)
+		themes = ['wordpressapp'];
+		for(i=0;i<themes.length;i++) {
+			name = themes[i];
+			shell.exec('rm -f ' + ls.wppath + '/wp-content/themes/' + name);
+			shell.exec('ln -sf ' + pwd + '/wp-content/themes/' + name + ' ' + ls.wppath + '/wp-content/themes/' + name);
+		}
+		shell.exec(wpcmd + 'theme activate wordpressapp');//
+
+
+		// our own, site-specific plugins
+		plugins = ['wordpressapp'];
+		for(i=0;i<plugins.length;i++) {
+			name = plugins[i];
+			shell.exec('rm -f ' + ls.wppath + '/wp-content/plugins/' + name);
+			shell.exec('ln -sf ' + pwd + '/wp-content/plugins/' + name + ' ' + ls.wppath + '/wp-content/plugins/' +name);
+			shell.exec(wpcmd + 'plugin activate ' + name);
+		}
+
+	});
+
+	grunt.registerTask('wp-export', '', function() {
+		ls = getLocalsettings();
+		wpcmd = 'wp --path=' + ls.wppath + ' --allow-root ';
+		pwd = shell.pwd();
+
+		// push settings from DB to file
+		src = ls.wppath + '/wp-content/config/wpbase_settings.json';
+		trg = pwd + '/bootstrap/wpbase_settings.json';
+		shell.exec(wpcmd + 'config push wpbase_settings');
+		shell.cp('-f', src, trg);
+	});
+
+	grunt.registerTask('wp-import', '', function() {
+		ls = getLocalsettings();
+		wpcmd = 'wp --path=' + ls.wppath + ' --allow-root ';
+		pwd = shell.pwd();
+
+		shell.mkdir('-p', ls.wppath + 'wp-content/config');
+
+		src = pwd + '/bootstrap/wpbase_settings.json';
+		trg = ls.wppath + '/wp-content/config/wpbase_settings.json';
+		shell.cp('-f', src, trg);
+		shell.exec(wpcmd + 'config pull wpbase_settings');
+	});
+
+	function getLocalsettings(test) {
+		var testMode = grunt.option('test');
+		if(test == true) {
+			testMode = true;
+		}
+		ls = grunt.file.readJSON('localsettings.json');
+		if(ls.wppath === undefined) ls.wppath = shell.pwd() + '/www/wordpress-default';
+		if(testMode == true) {
+			ls.environment = 'test';
+			ls.wppath = ls.wppath_test;
+			ls.dbname = ls.dbname_test;
+			ls.url = ls.url_test;
+		}
+		return ls;
+	}
+
+
+};
